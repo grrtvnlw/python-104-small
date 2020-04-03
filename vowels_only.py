a4bb0456ba747dfc8db983a0cf4fb657aff28848
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-03 15:53:42 -0400

    Improved comments

--- vowels_only.py
+++ vowels_only.py
@@ -2,8 +2,10 @@
 # get user input as a string // lowercase the input
 inp = input("Give me some text! ").lower()
 vowels = "aeiouy"
-
+# checks each letter in input
 for letter_inp in inp:
+# checks each letter in vowels
     for letter_vowels in vowels:
+# compares letter in vowels with letter in input
         if letter_inp == letter_vowels:
             print(letter_vowels)
