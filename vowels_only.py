d7ccd10ada7a2e16d928412e5184ea73a9b2e651
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-05 15:07:43 -0400

    Refactored Vowels Only exercise

--- vowels_only.py
+++ vowels_only.py
@@ -1,12 +1,10 @@
-# given text as a string, print the text in leetspeak
+# given text as a string, print only the vowels in the string
 # get user input as a string // lowercase the input
 inp = input("Give me some text! ").lower()
 vowels = "aeiouy"
-# checks each letter in input
-for letter_inp in inp:
-# checks each letter in vowels
-    for letter_vowels in vowels:
-# compares letter in vowels with letter in input
-        if letter_inp == letter_vowels:
+# iterates through each letter in input
+for letter in inp:
+# checks if each letter in input is in vowels
+    if letter in vowels:
 # prints letter only if it is a vowel
-            print(letter_vowels)
+            print(letter)
